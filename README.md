6bc0edf57249fce7543f5960475e4555773ea327
Erfan Golchini <erfan.golchini@esentri.com>
2023-05-14 11:42:47 +0200

    yep

--- README.md
+++ README.md
@@ -1,70 +1,33 @@
-# Getting Started with Create React App
+# Webcam Video Capture and Analysis
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project captures video and audio from your webcam, uploads it to a Supabase storage bucket, and sends the video to an API for analysis.
 
-## Available Scripts
+## Setup
 
-In the project directory, you can run:
+1. First, clone this repository to your local machine using `git clone`.
 
-### `npm start`
+2. Install all the necessary packages using `npm install`.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+3. In the `WebcamVideo.js` file, replace the `SUPABASE_URL`, `SUPABASE_KEY`, `ANALYSIS_URL`, `REPORT_URL`, `TOKEN`, and `STORAGE_BUCKET` constants with your own values.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Usage
 
-### `npm test`
+Run `npm start` to start the application. You'll be able to capture video and audio from your webcam.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Features
 
-### `npm run build`
+- **Start Capture:** Starts recording video from your webcam and audio from your microphone.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+- **Stop Capture:** Stops recording.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+- **Download:** Uploads the captured video to a Supabase storage bucket, sends the video to an API for analysis, and logs the response.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Tech Stack
 
-### `npm run eject`
+This project uses the following technologies:
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- React.js for the frontend.
+- react-webcam for webcam access.
+- Supabase for storing the recorded video.
+- uuid for generating unique identifiers for the videos.
+- Two different APIs for video analysis and generating reports.
--- src/WebcamVideo.js
+++ src/WebcamVideo.js
@@ -1,51 +1,112 @@
-import React from "react";
+import React, { useState, useRef, useCallback } from "react";
 import Webcam from "react-webcam";
+import { createClient } from '@supabase/supabase-js'
+import { v4 as uuidv4 } from 'uuid';
+
+// Constant URLs and Tokens
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY;
+const ANALYSIS_URL = process.env.REACT_APP_ANALYSIS_URL;
+const REPORT_URL = process.env.REACT_APP_REPORT_URL;
+const TOKEN = process.env.REACT_APP_TOKEN;
+const STORAGE_BUCKET = process.env.REACT_APP_STORAGE_BUCKET;
+const DID_URL = process.env.REACT_APP_DID_URL;
+
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export default function WebcamVideo() {
-  const webcamRef = React.useRef(null);
-  const mediaRecorderRef = React.useRef(null);
-  const [capturing, setCapturing] = React.useState(false);
-  const [recordedChunks, setRecordedChunks] = React.useState([]);
+  const webcamRef = useRef(null);
+  const mediaRecorderRef = useRef(null);
+  const [capturing, setCapturing] = useState(false);
+  const [recordedChunks, setRecordedChunks] = useState([]);
 
-  const handleDataAvailable = React.useCallback(
-    ({ data }) => {
-      if (data.size > 0) {
-        setRecordedChunks((prev) => prev.concat(data));
-      }
-    },
-    [setRecordedChunks]
-  );
+  const handleDataAvailable = useCallback(({ data }) => {
+    if (data.size > 0) setRecordedChunks((prev) => prev.concat(data));
+  }, []);
 
-  const handleStartCaptureClick = React.useCallback(() => {
+  const handleStartCaptureClick = useCallback(async () => {
     setCapturing(true);
-    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
-      mimeType: "video/webm",
-    });
-    mediaRecorderRef.current.addEventListener(
-      "dataavailable",
-      handleDataAvailable
-    );
+    
+    const audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const combinedStream = new MediaStream();
+    
+    // Add video track from webcam stream
+    if (webcamRef.current && webcamRef.current.stream) {
+      webcamRef.current.stream.getVideoTracks().forEach(track => combinedStream.addTrack(track));
+    }
+    
+    // Add audio track from audio stream
+    audioStream.getAudioTracks().forEach(track => combinedStream.addTrack(track));
+    
+    mediaRecorderRef.current = new MediaRecorder(combinedStream, { mimeType: "video/webm;codecs=vp9" });
+    mediaRecorderRef.current.addEventListener("dataavailable", handleDataAvailable);
     mediaRecorderRef.current.start();
-  }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
+  }, [webcamRef, handleDataAvailable]);
 
-  const handleStopCaptureClick = React.useCallback(() => {
+  const handleStopCaptureClick = useCallback(() => {
     mediaRecorderRef.current.stop();
     setCapturing(false);
-  }, [mediaRecorderRef, setCapturing]);
+  }, []);
 
-  const handleDownload = React.useCallback(() => {
+  const handleDownload = useCallback(async () => {
     if (recordedChunks.length) {
-      const blob = new Blob(recordedChunks, {
-        type: "video/webm",
-      });
-      const url = URL.createObjectURL(blob);
+      const blob = new Blob(recordedChunks, { type: "video/webm" });
+      const file = new File([blob], `${uuidv4()}.webm`, { type: "video/webm" });
+      const filename = `${uuidv4()}.mp4`;
+      const { error } = await supabase.storage.from(STORAGE_BUCKET).upload(filename, file);
+      if (error) console.error('Error uploading file:', error);
+      else {
+        const { data: { publicUrl } } = await supabase.storage.from(STORAGE_BUCKET).getPublicUrl(filename)
+        console.log("publicURL", publicUrl);
+
+        try {
+          const transcript = await fetch(ANALYSIS_URL, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              "video": publicUrl,
+              "token": TOKEN
+            }),
+          });
+          const firstData = await transcript.json();
+          console.log('Response:', firstData.message);;
+
+          const advice = await fetch(REPORT_URL, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              "message": firstData.message
+            }),
+          });
+          const adviceText = await advice.json();
+          console.log('Response:', adviceText.message);
+          const responseVideo = await fetch(DID_URL, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              "message": adviceText.message
+            }),
+          });
+          const Video= await responseVideo.json();
       const a = document.createElement("a");
       document.body.appendChild(a);
       a.style = "display: none";
-      a.href = url;
-      a.download = "react-webcam-stream-capture.webm";
+      a.href = Video.link;
+      a.download = Video.link;
       a.click();
-      window.URL.revokeObjectURL(url);
+      window.URL.revokeObjectURL(Video.link);
+      setRecordedChunks([]);
+        } catch (error) {
+          console.error(error);
+        }
+      }
       setRecordedChunks([]);
     }
   }, [recordedChunks]);
